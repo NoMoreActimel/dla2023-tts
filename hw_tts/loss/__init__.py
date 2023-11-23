0790b6c3066d388fae21f8848d659bf7479199a1
NoMoreActimel <evolution3000futurama@gmail.com>
2023-11-23 03:58:18 +0300

    loss import fix

--- hw_tts/loss/__init__.py
+++ hw_tts/loss/__init__.py
@@ -1,7 +1,9 @@
 from hw_tts.loss.CTCLossWrapper import CTCLossWrapper as CTCLoss
 from hw_tts.loss.SpExPlusLoss import SpExPlusLoss
+from hw_tts.loss.FastSpeech2Loss import FastSpeech2Loss
 
 __all__ = [
     "CTCLoss",
-    "SpExPlusLoss"
+    "SpExPlusLoss",
+    "FastSpeech2Loss"
 ]
